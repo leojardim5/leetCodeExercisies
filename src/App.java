2b7d3c516c5ab9998b9527cbde10120d6149c705
Leonardo Jardim <leojardim5@gmail.com>
2025-03-10 16:39:15 -0300

    resolvendo o exercicio de divisao de strings

--- src/App.java
+++ src/App.java
@@ -4,7 +4,15 @@ import java.util.HashMap;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String[] paraTeste = divideString("abcdefghij", 3, 'k');
+
+        for (String string : paraTeste) {
+
+            System.out.println(string);
+
+        }
+
     }
 
     public static int[] twoSum(int[] array, int target) {
@@ -26,6 +34,42 @@ public class App {
         }
 
         return null;
+    }
+
+    public static String[] divideString(String s, int k, char fill) {
+
+        int rest = s.length() % k;
+        int numberOfStrings = s.length() / k;
+
+        StringBuilder newString = new StringBuilder();
+
+        String[] arrayToReturn = (rest > 0) ? new String[numberOfStrings + 1] : new String[numberOfStrings];
+        int indexIterator = 0;
+
+        for (int x = 0, y = 0; x < s.length(); x++) {
+
+            newString.append(s.charAt(x));
+
+            if (newString.length() == k) {
+                arrayToReturn[indexIterator++] = newString.toString();
+                newString.setLength(0);
+
+            }
+
+        }
+
+        if (newString.length() > 0) {
+
+            while (newString.length() < k) {
+                newString.append(fill);
+            }
+
+            arrayToReturn[indexIterator] = newString.toString();
+
+        }
+
+        return arrayToReturn;
 
     }
+
 }
