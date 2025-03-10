667c7ef817fd7ba912ee0c7425f51cabd351bd2d
Leonardo Jardim <leojardim5@gmail.com>
2025-03-10 15:24:47 -0300

    implementando twoSum

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+
+import java.util.HashMap;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public static int[] twoSum(int[] array, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int x = 0; x < array.length; x++) {
+
+            int complement = target - array[x];
+
+            if (map.containsKey(complement)) {
+
+                return new int[]{map.get(complement), x};
+
+            }
+
+            map.put(array[x], x);
+
+        }
+
+        return null;
+
     }
 }
