f0937ebe5af6259b2ef7823c377cbe63f842051e
Leonardo Jardim <leojardim5@gmail.com>
2025-03-27 23:54:48 -0300

    muitos exercicios

--- src/App.java
+++ src/App.java
@@ -12,14 +12,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // System.out.println(frequenciaDeCaracteres("aavvvsbb"));
-        String[] palavras = {"ola", "puta", "amigo", "caca"};
-
-        for (String string : letrasRepetidas(palavras)) {
-
-            System.out.println(string);
-
-        }
-
+        // for (String string : letrasRepetidas(palavras)) {
+        //     System.out.println(string);
+        // }
+        // String[] palavras = {"passaro", "peixe", "gato", "peixe", "peixe"};
+        // for (int freq : trocarPorFrequencia(palavras)) {
+        //     System.out.println(freq);
+        // }
+        // System.out.println(primeiraLetraUnica("aabbcsefmmc"));
+        // int[] valores = {1, 2, 3, 4, 6, 7, 8, 9};
+        // System.out.println(numerosDuplicados(valores));
     }
 
     public static int[] twoSum(int[] array, int target) {
@@ -209,7 +211,6 @@ public class App {
                 if (!set.add(palavra.charAt(i))) {
                     eRepetido = true;
                 }
-
             }
 
             if (!eRepetido) {
@@ -217,9 +218,283 @@ public class App {
             }
 
         }
-
         return lista;
+    }
+
+    public static int[] trocarPorFrequencia(String[] palavras) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < palavras.length; i++) {
+
+            map.put(palavras[i], map.getOrDefault(palavras[i], 0) + 1);
+
+        }
+
+        int[] listaFrequencia = new int[map.size()];
+        int i = 0;
+        for (int frequencia : map.values()) {
+
+            listaFrequencia[i] = frequencia;
+            i++;
+        }
+        return listaFrequencia;
 
     }
+
+    public static Character primeiraLetraUnica(String frase) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < frase.length(); i++) {
+
+            map.put(frase.charAt(i), map.getOrDefault(frase.charAt(i), 0) + 1);
+
+        }
+
+        for (char x : frase.toCharArray()) {
+
+            if (map.get(x) == 1) {
+
+                return x;
+
+            }
+
+        }
+
+        return null;
+    }
+
+    public static boolean verificarAnagramas(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        }
+
+        for (char c : s2.toCharArray()) {
+
+            if (map.containsKey(c) || map.get(c) == 0) {
+
+                map.put(c, map.get(c) - 1);
+
+            }
+
+        }
+
+        for (Integer i : map.values()) {
+
+            if (i != 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public static int contarFreqImpar(int[] vetor) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : vetor) {
+
+            map.put(i, map.getOrDefault(i, 0) + 1);
+
+        }
+
+        for (int x : vetor) {
+
+            if (map.get(x) % 2 != 0) {
+
+                return x;
+
+            }
+
+        }
+        return -1;
+    }
+
+    public static List<String> palavraComLetraRepetida(String[] vetorPalavras) {
+
+        List<String> listToReturn = new ArrayList<>();
+
+        for (int x = 0; x < vetorPalavras.length; x++) {
+
+            HashSet<Character> set = new HashSet<>();
+            for (char c : vetorPalavras[x].toCharArray()) {
+
+                if (!set.add(c)) {
+
+                    listToReturn.add(vetorPalavras[x]);
+                    break;
+
+                }
+
+            }
+
+        }
+
+        return listToReturn;
+
+    }
+
+    public static boolean palindromoPorOrdenacao(String palavra) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : palavra.toCharArray()) {
+
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        }
+        int contadorImpar = 0;
+        for (int c : map.values()) {
+
+            if (palavra.length() % 2 == 0) {
+
+                if (c % 2 != 0) {
+                    return false;
+                }
+
+            }
+
+            if (palavra.length() % 2 != 0) {
+
+                if (c % 2 != 0) {
+                    contadorImpar++;
+                }
+
+            }
+
+            if (contadorImpar > 1) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public static int caracteresEmComum(String s1, String s2) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+
+            map.put(c, 1);
+
+        }
+
+        for (char c : s2.toCharArray()) {
+
+            if (map.getOrDefault(c, 0) == 1) {
+
+                map.put(c, 2);
+
+            }
+
+        }
+
+        int valueToReturn = 0;
+
+        for (int c : map.values()) {
+
+            if (c == 2) {
+                valueToReturn++;
+            }
+
+        }
+
+        return valueToReturn;
+
+    }
+
+    public static boolean ePangrama(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c != ' ') {
+
+                set.add(c);
+
+            }
+
+        }
+
+        return set.size() == 26 ? true : false;
+    }
+
+    public static boolean numerosDuplicados(int[] valores) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int valor : valores) {
+
+            if (!set.add(valor)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static List<Integer> intersecDoisArrays(int[] nums1, int[] nums2) {
+        Set<Integer> listaToReturn = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums1) {
+            set.add(n);
+        }
+
+        for (int n : nums2) {
+
+            if (set.contains(n)) {
+                listaToReturn.add(n);
+            }
+
+        }
+
+        return new ArrayList<>(listaToReturn);
+
+    }
+
+    public static int maiorApperance(int[] vetor) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : vetor) {
+
+            map.put(i, map.getOrDefault(i, 0) + 1);
+
+        }
+        int maiorValor = 0;
+        int maiorAparicao = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+            if (entry.getValue() > maiorValor) {
+                maiorAparicao = entry.getKey();
+                maiorValor = entry.getValue();
+            }
+
+        }
+
+        return maiorAparicao;
+
+    }
+
+    
 
 }
